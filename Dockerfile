f6609ce9377f8bc06cd727a1bcde19e1953ae999
crocandr <slezsak.andras@gmail.com>
2020-04-09 11:48:23 +0200

    some dev updates to core version

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,7 @@
 FROM debian:buster
 
-ENV DOWNLOAD_URL https://unimus.net/download-unimus/-%20Latest/Unimus.jar
-
-# Check the https://github.com/crocandr/docker-unimus/issues/4 for download URLs.
-# ENV DOWNLOAD_URL https://unimus.net/download-unimus/1.10.3/Unimus.jar
+ENV DOWNLOAD_URL https://unimus.net/download-unimus-core/dev-builds/Unimus-Core.jar
+#ENV DOWNLOAD_URL https://unimus.net/download-unimus-core/-%20Latest/Unimus-Core.jar
 
 RUN apt-get update && apt-get install -y curl vim less wget tzdata
 
@@ -11,7 +9,7 @@ RUN apt-get update && apt-get install -y curl vim less wget tzdata
 RUN apt-get install -y openjdk-11-jre-headless
 #
 # Unimus 
-RUN curl -L -o /opt/unimus.jar $DOWNLOAD_URL
+RUN curl -L -o /opt/unimus-core.jar $DOWNLOAD_URL
 #
 # Start script
 COPY files/start.sh /opt/start.sh
--- files/start.sh
+++ files/start.sh
@@ -2,6 +2,13 @@
 
 JAVA_EXTRA_PARAMS=""
 
+CONFIG_FILE="/etc/unimus-core/unimus-core.properties"
+
+# default value of unimus server port
+[ -z $UNIMUS_SERVER_ADDRESS ] && { UNIMUS_SERVER_ADDRESS=172.17.0.1; }
+[ -z $UNIMUS_SERVER_PORT ] && { UNIMUS_SERVER_PORT=8085; }
+[ -z $UNIMUS_SERVER_ACCESS_KEY ] && { UNIMUS_SERVER_ACCESS_KEY="NOT_A_VALID_ACCESS_KEY"; }
+
 [ ! -z "$XMS" ] &&  { JAVA_EXTRA_PARAMS="$JAVA_EXTRA_PARAMS -Xms$XMS"; }
 [ ! -z "$XMX" ] && { JAVA_EXTRA_PARAMS="$JAVA_EXTRA_PARAMS -Xmx$XMX"; }
 [ ! -z "$JAVA_OPTS" ] && { JAVA_EXTRA_PARAMS="$JAVA_OPTS"; }
@@ -10,5 +17,12 @@ JAVA_EXTRA_PARAMS=""
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
 
+[ ! -f $CONFIG_FILE ] && { touch $CONFIG_FILE; }
+[ ! -z $UNIMUS_SERVER_ADDRESS ] && { sed -i s@unimus.address=.*@unimus.address=$UNIMUS_SERVER_ADDRESS@g $CONFIG_FILE; }
+[ $( grep -i "unimus.port=" $CONFIG_FILE | wc -l ) -eq 0 ] && { echo "unimus.port=" >> $CONFIG_FILE; } 
+[ ! -z $UNIMUS_SERVER_PORT ] && { sed -i s@unimus.port=.*@unimus.port=$UNIMUS_SERVER_PORT@g $CONFIG_FILE; }
+[ $( grep -i "unimus.access.key=" $CONFIG_FILE | wc -l ) -eq 0 ] && { echo "unimus.access.key=" >> $CONFIG_FILE; }
+[ ! -z $UNIMUS_SERVER_ACCESS_KEY ] && { sed -i s@unimus.access.key=.*@unimus.access.key=$UNIMUS_SERVER_ACCESS_KEY@g $CONFIG_FILE; }
+
 # run
-java $JAVA_EXTRA_PARAMS -jar /opt/unimus.jar
+java $JAVA_EXTRA_PARAMS -jar /opt/unimus-core.jar
